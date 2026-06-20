8be740c8d40eeef39bd764bc2e42e0571ea1c863
sandyy2020 <sandeep.prithvi2070@gmail.com>
2026-06-20 17:15:14 +0530

    app.jsx update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
-// import Router from "./Router";
+// import App from "./App";
+import Router from "./Router";
 
 
 createRoot(document.getElementById("app"))
 .render(
     <BrowserRouter>
-        <App />
-        {/* <Router /> */}
+        {/* <App /> */}
+        <Router />
     </BrowserRouter>
 );
