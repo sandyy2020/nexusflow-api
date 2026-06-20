ceac7a83cd7f7f357cd45abd48c8f5d5b9eefe9c
sandyy2020 <sandeep.prithvi2070@gmail.com>
2026-06-20 16:07:42 +0530

    api.php update

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'Nexus Api is Running and used for testing purpose only',
-    ]);
+// Route::get('/test', function () {
+//     return response()->json([
+//         'message' => 'Nexus Api is Running and used for testing purpose only',
+//     ]);
 
-});
+// });
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
